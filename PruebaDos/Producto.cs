c0605d9c71b0af1008c11c61af90aebed8888db6
javieradp20 <110201761+javieradp20@users.noreply.github.com>
2022-11-20 18:04:25 -0300

    Agregar archivos de proyecto.

---
+++ PruebaDos/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaDos
+{
+    internal class Producto
+    {
+        public int CodigoProducto { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public string Descripcion { get; set; }
+        public bool Dpisponibilidad { get; set; }
+        public string ClienteId { get; set; }
+
+    }
+}
